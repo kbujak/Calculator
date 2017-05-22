2b3ac4e1e78c4d13ec63dfbae566aa9c11847e08
Krystian <kbujak421@gmail.com>
2017-05-22 13:45:30 +0200

    Added Controller and View

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var isUserInTheMiddle = false
+    //let calculatorBrain = CalculatorBrain()
+    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
 
+    @IBAction func touchDigt(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isUserInTheMiddle{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            isUserInTheMiddle = true
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+    }
 }
 
